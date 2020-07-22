58b7292e2217c310bb53f57eb1622be9be7169f1
hawjeh <hawjeh.lee@websparks.sg>
2020-07-22 17:16:11 +0800

    webapp1 and webapp2

---
+++ WebApp1/Controllers/WebOneController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebOneController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello Web One!";
+        }
+
+        [HttpGet("One")]
+        public string One()
+        {
+            return "Hello Web One One!";
+        }
+    }
+}
---
+++ WebApp2/Controllers/WebTwoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebTwoController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello Web Two!";
+        }
+    }
+}
