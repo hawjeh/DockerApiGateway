7f133d62ceaecb6bf14cf66887c71742e8e64527
hawjeh <hawjeh.lee@websparks.sg>
2020-07-22 18:05:19 +0800

    api gateway and two webapp

---
+++ Gateway/Model/Destination.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.Model
+{
+    public class Destination
+    {
+        public string Uri { get; set; }
+        public bool RequiresAuthentication { get; set; }
+        static readonly HttpClient client = new HttpClient();
+
+        public async Task<HttpResponseMessage> SendRequest(HttpRequest request)
+        {
+            string requestContent;
+            using (Stream receiveStream = request.Body)
+            {
+                using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                {
+                    requestContent = await readStream.ReadToEndAsync();
+                }
+            }
+
+            using (var newRequest = new HttpRequestMessage(new HttpMethod(request.Method), CreateDestinationUri(request)))
+            {
+                newRequest.Content = new StringContent(requestContent, Encoding.UTF8, request.ContentType);
+                return await client.SendAsync(newRequest);
+                //using (var response = await client.SendAsync(newRequest))
+                //{
+                //    return response;
+                //}
+            }
+        }
+
+        private string CreateDestinationUri(HttpRequest request)
+        {
+            string requestPath = request.Path.ToString();
+            string queryString = request.QueryString.ToString();
+
+            string endpoint = "";
+            string[] endpointSplit = requestPath.Substring(1).Split('/');
+
+            if (endpointSplit.Length > 1)
+                endpoint = endpointSplit[1];
+
+
+            return Uri + endpoint + queryString;
+        }
+
+    }
+}
---
+++ Gateway/Model/Route.cs
@@ -0,0 +1,8 @@
+﻿namespace Gateway.Model
+{
+    public class Route
+    {
+        public string Endpoint { get; set; }
+        public Destination Destination { get; set; }
+    }
+}
